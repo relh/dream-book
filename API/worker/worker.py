f6f0fcf8546a5e368ae9009bc6746a8793341819
shariqhashme@gmail.com <ubuntu@ip-172-31-44-119.us-west-2.compute.internal>
2015-07-15 19:54:02 +0000

    cleaned up; initial version of worker

--- API/worker/worker.py
+++ API/worker/worker.py
@@ -1,101 +1,51 @@
-#!/usr/bin/env python
+# make sure to configure AWS from the CLI first!
+import boto3
+import PIL.Image
+import urllib2
+from cStringIO import StringIO
+from multiprocessing import Queue
+from multiprocessing.managers import BaseManager
 
-import datetime
-import threading
-import time
-import json
-import random
-import re
-from Queue import Queue
-from bottle import route, hook, response, run, static_file, request
+# dreambaby is a generator which yields StringIO buffers containing 
+# partial deep dreams, until the last one, the full deep dream
+# it takes in a PIL Image and template
+from deepdream import dreambaby
 
-data_structure = []
-# {input: url, status: done, output: url}
+# grab the queue
+AUTH_KEY = 'changeinprod'
+QUEUE_IP = '127.0.0.1'
 
-workers = ['127.0.0.1']
-workers_available = {}
-for worker in workers:workers_available[worker] = True
 
-workerthreads = []
-todo = Queue()
-
-def dowork(worker_ip, input_image):
-    pass
-    # hit worker_ip with this request
-
-def workerThread(worker_ip):
-    while True:
-        time.sleep(0.5)
-        if workers_available[worker_ip]:
-            dowork(worker_ip, todo.get())
-            workers_available[worker_ip] = False
-
-for worker in workers:
-    workert = threading.Thread(target = workerThread, args = (worker, ))
-    workerthreads.append(workert)
-    workert.daemon = True
-    workert.start()
-
-try:
-    thing = open('data.bkp').read()
-    data_structure = eval(thing)
-except:
+class Manager(BaseManager):
+    ''' manager which connects to a remote queue '''
     pass
 
-def backupData():
+
+Manager.register('getQueue')
+MANAGER = Manager(address=(QUEUE_IP, 6200), authkey=AUTH_KEY)
+MANAGER.connect()
+MANAGED_QUEUE = MANAGER.getQueue()
+
+
+S3 = boto3.resource('s3')
+BUCKET_NAME = 'deepdreambook'
+
+
+def url_to_image(url):
+    ''' takes in a url, returns a PIL.Image '''
+    buffer = StringIO(urllib2.urlopen(url).read())
+    return PIL.Image.open(buffer)
+
+
+def push_buffer(buffer, key):
+    ''' pushes a StringIO to S3 '''
+    S3.Bucket(BUCKET_NAME).put_object(Key=key, Body=buffer)
+
+
+if __name__ == '__main__':
     while True:
-        time.sleep(30)
-        open('data.bkp','w').write(str(data_structure))
-
-url_cache = {}
-
-for c, item in enumerate(data_structure):
-    url_cache[item['input']] = c
-
-backerupper = threading.Thread(target = backupData)
-backerupper.daemon = True
-backerupper.start()
-
-
-@route('/')
-def index():
-    return 'Not found'
-
-@route('/push', method = 'GET')
-def push():
-    url = request.params.get('url','')
-    # does url need to be urldecoded
-    if 'http' not in url:
-        return 'Error'
-    if url in url_cache:
-        return url_cache[url]
-    url_cache[url] = len(data_structure)
-    data_structure.append({'input':url, 'output':''})
-    todo.put(url)
-    return url_cache[url]
-
-@route('/status/<place>')
-def status(place='0'):
-    try:
-        numplace = int(place)
-    except:
-        return 'Error'
-    if len(data_structure) > numplace:
-        return data_structure[numplace]['output']
-    return 'Error'
-
-@route('/update/<place>/<output>')
-def update(place='x', output='x'):
-    # how does remote ip look for localhost
-    remote_ip = request.environ.get('REMOTE_ADDR')
-    print remote_ip
-    try:
-        numplace = int(place)
-        if remote_ip not in workers:
-            raise
-    except:
-        return 'Error'
-    data_structure[numplace]['output'] = output
-    workers_available[remote_ip] = True
-
-run(host = '0.0.0.0', port = 8080, server = 'tornado')
+        token, template, url = MANAGED_QUEUE.get()
+        image = url_to_image(url)
+        generator = dreambaby(image, template)
+        for counter, partial in enumerate(generator):
+            push_buffer(partial, token + str(counter))
